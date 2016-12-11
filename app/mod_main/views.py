a37fb3a73c968d99aad82a77a61884d82968a772
Diamant <diamanthaxhimusa@gmail.com>
2016-12-11 20:36:52 +0100

    docs added , pymongo and html , new route search by id in mongo db

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,11 +1,25 @@
 from flask import Blueprint, render_template, request
+from app import mongo
+from bson import json_util, ObjectId
 
 mod_main = Blueprint('main', __name__)
 
-
 @mod_main.route('/', methods=['GET','POST'])
 def index():
-  if request.method == 'GET':
-    return render_template('index.html')
-  elif request.method == 'POST':
-    return render_template('index.html')
+    db = mongo.db.arkep
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        data = request.form.to_dict()
+        db.insert(data)
+
+@mod_main.route('/<string:id>', methods=['GET'])
+def get_doc(id):
+    db = mongo.db.arkep
+    if request.method == 'GET':
+        doc = db.find_one({"_id":ObjectId(id)})
+
+        doc_json = json_util.dumps(doc)
+        return render_template('doc.html', doc=doc)
+    else:
+        return "bad request"
